a286f71dc86325ba5704e2978bd184b912462931
yg.song <insaint03@gmail.com>
2020-10-18 20:27:36 +0900

    images updated

--- src/assets/gallery/gallery.js
+++ src/assets/gallery/gallery.js
@@ -44,6 +44,86 @@ import gallery_43 from './43.jpg'
 import gallery_44 from './44.jpg'
 import gallery_45 from './45.jpg'
 
+import gallery_2_01 from './g2_01.jpg'
+import gallery_2_02 from './g2_02.jpg'
+import gallery_2_03 from './g2_03.jpg'
+import gallery_2_04 from './g2_04.jpg'
+import gallery_2_05 from './g2_05.jpg'
+import gallery_2_06 from './g2_06.jpg'
+import gallery_2_07 from './g2_07.jpg'
+import gallery_2_08 from './g2_08.jpg'
+import gallery_2_09 from './g2_09.jpg'
+import gallery_2_10 from './g2_10.jpg'
+import gallery_2_11 from './g2_11.jpg'
+import gallery_2_12 from './g2_12.jpg'
+import gallery_2_13 from './g2_13.jpg'
+import gallery_2_14 from './g2_14.jpg'
+import gallery_2_15 from './g2_15.jpg'
+import gallery_2_16 from './g2_16.jpg'
+import gallery_2_17 from './g2_17.jpg'
+import gallery_2_18 from './g2_18.jpg'
+import gallery_2_19 from './g2_19.jpg'
+import gallery_2_20 from './g2_20.jpg'
+import gallery_2_21 from './g2_21.jpg'
+import gallery_2_22 from './g2_22.jpg'
+import gallery_2_23 from './g2_23.jpg'
+import gallery_2_24 from './g2_24.jpg'
+import gallery_2_25 from './g2_25.jpg'
+import gallery_2_26 from './g2_26.jpg'
+import gallery_2_27 from './g2_27.jpg'
+import gallery_2_28 from './g2_28.jpg'
+import gallery_2_29 from './g2_29.jpg'
+import gallery_2_30 from './g2_30.jpg'
+import gallery_2_31 from './g2_31.jpg'
+import gallery_2_32 from './g2_32.jpg'
+import gallery_2_33 from './g2_33.jpg'
+import gallery_2_34 from './g2_34.jpg'
+import gallery_2_35 from './g2_35.jpg'
+import gallery_2_36 from './g2_36.jpg'
+import gallery_2_37 from './g2_37.jpg'
+import gallery_2_38 from './g2_38.jpg'
+import gallery_2_39 from './g2_39.jpg'
+import gallery_2_40 from './g2_40.jpg'
+import gallery_2_41 from './g2_41.jpg'
+import gallery_2_42 from './g2_42.jpg'
+import gallery_2_43 from './g2_43.jpg'
+import gallery_2_44 from './g2_44.jpg'
+import gallery_2_45 from './g2_45.jpg'
+import gallery_2_46 from './g2_46.jpg'
+import gallery_2_47 from './g2_47.jpg'
+import gallery_2_48 from './g2_48.jpg'
+import gallery_2_49 from './g2_49.jpg'
+import gallery_2_50 from './g2_50.jpg'
+import gallery_2_51 from './g2_51.jpg'
+import gallery_2_52 from './g2_52.jpg'
+import gallery_2_53 from './g2_53.jpg'
+import gallery_2_54 from './g2_54.jpg'
+import gallery_2_55 from './g2_55.jpg'
+import gallery_2_56 from './g2_56.jpg'
+import gallery_2_57 from './g2_57.jpg'
+import gallery_2_58 from './g2_58.jpg'
+import gallery_2_59 from './g2_59.jpg'
+import gallery_2_60 from './g2_60.jpg'
+import gallery_2_61 from './g2_61.jpg'
+import gallery_2_62 from './g2_62.jpg'
+import gallery_2_63 from './g2_63.jpg'
+import gallery_2_64 from './g2_64.jpg'
+import gallery_2_65 from './g2_65.jpg'
+import gallery_2_66 from './g2_66.jpg'
+import gallery_2_67 from './g2_67.jpg'
+import gallery_2_68 from './g2_68.jpg'
+import gallery_2_69 from './g2_69.jpg'
+import gallery_2_70 from './g2_70.jpg'
+import gallery_2_71 from './g2_71.jpg'
+import gallery_2_72 from './g2_72.jpg'
+import gallery_2_73 from './g2_73.jpg'
+import gallery_2_74 from './g2_74.jpg'
+import gallery_2_75 from './g2_75.jpg'
+import gallery_2_76 from './g2_76.jpg'
+import gallery_2_77 from './g2_77.jpg'
+import gallery_2_78 from './g2_78.jpg'
+
+
 
 
 
@@ -76,7 +156,7 @@ export default {
     ],},
   ],
   images: [
-    // {asset: rect01, tags: ['ht', 'wd'], desc: 'Lorem' }, 
+    // init: 2020.10.03.
     {asset: gallery_1, tags: ['담은꽃','6월','선물','웨딩'], desc: ''}, 
     {asset: gallery_2, tags: ['묶은꽃','3월','선물'], desc: ''}, 
     {asset: gallery_3, tags: ['담은꽃','5월','선물'], desc: ''}, 
@@ -122,5 +202,84 @@ export default {
     {asset: gallery_43, tags: ['묶은꽃','2월','선물'], desc: ''}, 
     {asset: gallery_44, tags: ['담은꽃','7월','선물','프로젝트'], desc: ''}, 
     {asset: gallery_45, tags: ['묶은꽃','3월','선물','웨딩'], desc: ''}, 
+    // season 2 : 2020.10.18.
+    {asset: gallery_2_01, tags: ['묶은꽃', '6월', '선물'], desc: '', },
+    {asset: gallery_2_02, tags: ['묶은꽃', '10월', '11월', '선물'], desc: '', },
+    {asset: gallery_2_03, tags: ['꽂은꽃', '4월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_04, tags: ['담은꽃', '7월', '선물'], desc: '', },
+    {asset: gallery_2_05, tags: ['담은꽃', '4월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_06, tags: ['꽂은꽃', '공간꽃', '5월','프로젝트'], desc: '', },
+    {asset: gallery_2_07, tags: ['꽂은꽃', '공간꽃', '6월','프로젝트'], desc: '', },
+    {asset: gallery_2_08, tags: ['꽂은꽃', '3월', '프로젝트' ], desc: '', },
+    {asset: gallery_2_09, tags: ['묶은꽃', '10월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_10, tags: ['공간꽃', '5월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_11, tags: ['꽂은꽃', '공간꽃', '11월','프로젝트'], desc: '', },
+    {asset: gallery_2_12, tags: ['묶은꽃', '7월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_13, tags: ['담은꽃', '10월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_14, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_15, tags: ['꽂은꽃', '공간꽃', '10월','프로젝트'], desc: '', },
+    {asset: gallery_2_16, tags: ['꽂은꽃', '9월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_17, tags: ['묶은꽃', '2월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_18, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_19, tags: ['묶은꽃', '4월', '선물'], desc: '', },
+    {asset: gallery_2_20, tags: ['담은꽃', '10월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_21, tags: ['꽂은꽃', '공간꽃', '11월','프로젝트'], desc: '', },
+    {asset: gallery_2_22, tags: ['묶은꽃', '6월', '선물'], desc: '', },
+    {asset: gallery_2_23, tags: ['꽂은꽃', '9월', '프로젝트'], desc: '', },
+    {asset: gallery_2_24, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_25, tags: ['꽂은꽃', '공간꽂', '6월','프로젝트'], desc: '', },
+    {asset: gallery_2_26, tags: ['꽂은꽃', '공간꽃', '6월','프로젝트'], desc: '', },
+    {asset: gallery_2_27, tags: ['담은꽃', '6월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_28, tags: ['묶은꽃', '12월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_29, tags: ['묶은꽃', '1월', '2월','선물', '웨딩'], desc: '', },
+    {asset: gallery_2_30, tags: ['꽂은꽃', '5월', '프로젝트'], desc: '', },
+    {asset: gallery_2_31, tags: ['담은꽃', '3월', '4월', '프로젝트'], desc: '', },
+    {asset: gallery_2_32, tags: ['꽂은꽃', '공간꽃', '5월', '프로젝트'], desc: '', },
+    {asset: gallery_2_33, tags: ['묶은꽃', '10월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_34, tags: ['묶은꽃', '7월', '웨딩'], desc: '', },
+    {asset: gallery_2_35, tags: ['묶은꽃', '2월', '	선물', '웨딩'], desc: '', },
+    {asset: gallery_2_36, tags: ['묶은꽃', '3월', '4월','웨딩'], desc: '', },
+    {asset: gallery_2_37, tags: ['담은꽃', '10월', '선물', '프로젝트'], desc: '', },
+    {asset: gallery_2_38, tags: ['묶은꽃', '4월', '웨딩'], desc: '', },
+    {asset: gallery_2_39, tags: ['묶은꽃', '4월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_40, tags: ['묶은꽃', '1월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_41, tags: ['묶은꽃', '10월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_42, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_43, tags: ['묶은꽃', '3월', '4월','선물', '웨딩'], desc: '', },
+    {asset: gallery_2_44, tags: ['묶은꽃', '2월', '선물'], desc: '', },
+    {asset: gallery_2_45, tags: ['묶은꽃', '4월', '5월','선물', '웨딩'], desc: '', },
+    {asset: gallery_2_46, tags: ['묶은꽃', '2월', '3월','선물', '웨딩'], desc: '', },
+    {asset: gallery_2_47, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_48, tags: ['묶은꽃', '9월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_49, tags: ['묶은꽃', '5월', '선물'], desc: '', },
+    {asset: gallery_2_50, tags: ['담은꽃', '6월', '프로젝트'], desc: '', },
+    {asset: gallery_2_51, tags: ['묶은꽃', '4월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_52, tags: ['묶은꽃', '6월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_53, tags: ['묶은꽃', '7월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_54, tags: ['묶은꽃', '8월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_55, tags: ['묶은꽃', '7월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_56, tags: ['묶은꽃', '2월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_57, tags: ['꽂은꽃', '3월', '프로젝트'], desc: '', },
+    {asset: gallery_2_58, tags: ['묶은꽃', '3월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_59, tags: ['꽂은꽃', '공간꽃', '4월', '5월','프로젝트'], desc: '', },
+    {asset: gallery_2_60, tags: ['꽂은꽃', '공간꽃', '9월','프로젝트'], desc: '', },
+    {asset: gallery_2_61, tags: ['묶은꽃', '9월', '선물'], desc: '', },
+    {asset: gallery_2_62, tags: ['꽂은꽃', '공간꽃', '4월', '5월','프로젝트'], desc: '', },
+    {asset: gallery_2_63, tags: ['꽂은꽃', '공간꽃', '4월', '5월','프로젝트'], desc: '', },
+    {asset: gallery_2_64, tags: ['꽂은꽃', '공간꽃', '9월','프로젝트'], desc: '', },
+    {asset: gallery_2_65, tags: ['공간꽃', '9월', '프로젝트'], desc: '', },
+    {asset: gallery_2_66, tags: ['묶은꽃', '8월', '9월','선물', '웨딩'], desc: '', },
+    {asset: gallery_2_67, tags: ['묶은꽃', '8월', '선물'], desc: '', },
+    {asset: gallery_2_68, tags: ['꽂은꽃', '공간꽃', '6월', '프로젝트'], desc: '', },
+    {asset: gallery_2_69, tags: ['꽂은꽃', '공간꽃', '10월', '11월','프로젝트'], desc: '', },
+    {asset: gallery_2_70, tags: ['꽂은꽃', '공간꽃', '8월','프로젝트'], desc: '', },
+    {asset: gallery_2_71, tags: ['꽂은꽃', '공간꽃', '3월','프로젝트'], desc: '', },
+    {asset: gallery_2_72, tags: ['묶은꽃', '8월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_73, tags: ['꽂은꽃', '공간꽃', '10월','프로젝트'], desc: '', },
+    {asset: gallery_2_74, tags: ['담은꽃', '4월', '5월','선물'], desc: '', },
+    {asset: gallery_2_75, tags: ['담은꽃', '7월', '선물'], desc: '', },
+    {asset: gallery_2_76, tags: ['묶은꽃', '10월', '선물'], desc: '', },
+    {asset: gallery_2_77, tags: ['묶은꽃', '2월', '선물', '웨딩'], desc: '', },
+    {asset: gallery_2_78, tags: ['묶은꽃', '2월', '선물', '웨딩'], desc: '', },
   ]
 }
